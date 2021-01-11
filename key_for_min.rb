006a64872951bb569baf3a11784a0b0de5a9e862
Chris Follen <christopher.follen@gmail.com>
2021-01-11 16:01:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  current_min = 1000000
+  current_min = nil
   min_key = nil
   name_hash.each do |key, value|
-    #if current_min == nil
+    if current_min == nil
       #current_min = value
       #min_key = key
-    #end
+    end
     if value < current_min
       current_min = value
       min_key = key
